d498cb92a3dab03fd5b279f7d788c5c35129c8bc
PatrickBezolfe <114448993+PatrickBezolfe@users.noreply.github.com>
2023-07-22 18:02:09 -0300

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -8,6 +8,7 @@ function clickMenu() {
         jobs.style.display = "none"
         services.style.display = "none"
         contact.style.display = "none"
+        
     }
 }
 
@@ -32,9 +33,61 @@ function clickOpen() {
         jobs.style.display = "block"
         services.style.display = "flex"
         contact.style.display = "flex"
+        
     }
 }
 
-function clickPort() {
+
+
+window.addEventListener('scroll', function() {
+    var scrollButton = document.getElementById('icon-up');
+    var section2 = document.querySelector('.capa-main');
+    var rect = section2.getBoundingClientRect();
     
+    if (rect.top <= 0) {
+      scrollButton.style.display = 'block'; // Mostra o botão ao rolar para a segunda seção
+    } else {
+      scrollButton.style.display = 'none'; // Oculta o botão na primeira seção
+    }
+})
+
+window.addEventListener('scroll', function() {
+    var scrollButton = document.getElementById('icon-up');
+    var section2 = document.querySelector('.esportes');
+    var rect = section2.getBoundingClientRect();
+    
+    if (rect.top <= 0) {
+      scrollButton.style.display = 'block'; // Mostra o botão ao rolar para a segunda seção
+    } else {
+      scrollButton.style.display = 'none'; // Oculta o botão na primeira seção
+    }
+})
+
+window.addEventListener('scroll', function() {
+    var scrollButton = document.getElementById('icon-up');
+    var section2 = document.querySelector('.music');
+    var rect = section2.getBoundingClientRect();
+    
+    if (rect.top <= 0) {
+      scrollButton.style.display = 'block'; // Mostra o botão ao rolar para a segunda seção
+    } else {
+      scrollButton.style.display = 'none'; // Oculta o botão na primeira seção
+    }
+})
+
+
+let count = 1;
+document.getElementById("carrossel1").checked = true;
+
+setInterval( function() {
+    nextImage();
+}, 5000)
+
+function nextImage() {
+    count++;
+    if (count > 6){
+        count = 1;
+    }
+
+    document.getElementById("carrossel"+count).checked = true;
 }
